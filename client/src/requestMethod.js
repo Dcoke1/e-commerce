517b382d22dd8f0b87f356b6efc8ab7b3dbc1228
Douglas Coke <easyg@G-Macbook-Pro.local>
2022-04-11 17:32:48 -0400

    update_enironment_variables

--- client/src/requestMethod.js
+++ client/src/requestMethod.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:4000/api/";
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNDM3YWExZDNiNjBlOTRlZGM5NWViZSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0OTA2MjAzMiwiZXhwIjoxNjQ5MzIxMjMyfQ.F_QkXwWy72xmRIAfXTpukpMFidkpDPsjHv8XYwsvxQ0";
+const url = process.env.REACT_APP_BASE_URL;
+const token = process.env.REACT_APP_TOKEN;
 
 export const publicRequest = axios.create({
-  baseURL: BASE_URL,
+  baseURL: url,
 });
 
 export const userRequest = axios.create({
-  baseURL: BASE_URL,
-  header: { token: `Bearer ${TOKEN}` },
+  baseURL: url,
+  header: { token: `Bearer ${token}` },
 });
